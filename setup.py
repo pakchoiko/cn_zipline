e93a337d84e13e2810c1735dfaeff7e2dc304a2a
JaysonAlbert <JaysonAlbert@users.noreply.github.com>
2017-11-10 12:32:47 +0800

    按手下单，整百倍数

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except (IOError, ImportError):
 
 setup(
     name='cn-zipline',
-    version='0.32',
+    version='0.33',
     description='china zipline bundles',
     long_description=long_description,
     author='Jie Wang',
